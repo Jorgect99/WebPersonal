69c2c64d7270aa1411a0d9a7f5868e38b253d8e6
Jorge Cabral <jorgect99@gmail.com>
2020-12-08 22:30:49 -0700

    Patron MVT

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
@@ -6,9 +6,6 @@ def home(request):
 
 def about(request):
     return render(request, 'core/about-me.html')
-
-def portfolio(request):
-    return render(request, 'core/portfolio.html')
 
 def contact(request):
     return render(request, 'core/contact.html')
